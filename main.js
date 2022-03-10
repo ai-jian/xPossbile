8397680885e6bdf4d456cc68c68d5341a99adc65
Ai Jian <53556553+ai-jian@users.noreply.github.com>
2022-03-09 23:00:41 -0600

    added landing animation

--- main.js
+++ main.js
@@ -11,3 +11,67 @@ toggleBtn.addEventListener('click', function () {
     bars.classList.toggle('toggled')
     navbar.classList.toggle('show-nav')
 })
+
+//ticker
+
+gsap.set('.ticker-wrapper', { xPercent: -50 })
+const boxWidth = '540',
+    totalWidth = boxWidth * 4,  //  * n of boxes
+    ticker = document.querySelectorAll("#ticker .box"),
+    dirFromLeft = "+=" + totalWidth,
+    dirFromRight = "-=" + totalWidth;
+
+const mod = gsap.utils.wrap(0, totalWidth);
+
+function marquee(which, time, direction) {
+    gsap.set(which, {
+        x: function (i) {
+            return i * boxWidth;
+        }
+    });
+    const action = gsap.timeline()
+        .to(which, {
+            x: direction,
+            modifiers: {
+                x: x => mod(parseFloat(x)) + "px"
+            },
+            duration: time, ease: 'none',
+            repeat: -1,
+        });
+    return action
+}
+
+const master = gsap.timeline()
+    .add(marquee(ticker, 20, dirFromLeft), 1)
+
+
+
+
+//landing
+gsap.registerPlugin(ScrollTrigger);
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".landing-left",
+        start: 'top 30%',
+        end: "center 100px",
+        markers: true,
+        scrub: 3,
+        toggleActions: "restart pause reverse pause"
+    }
+})
+
+tl
+    .to('.landing-left', {
+        x: -700,
+        opacity: 0,
+        duration: 3
+    })
+    .to('.landing-right', {
+        x: 700,
+        opacity: 0,
+        duration: 3
+    }, 0)
+    .to('#showcase', {
+        y: 0,
+    })
+
